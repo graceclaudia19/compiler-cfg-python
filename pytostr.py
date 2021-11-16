adaf367604f5eff96b5ffe0a1537c9fa2e3fd302
graceclaudia19 <graceclaudia78@gmail.com>
2021-11-16 18:05:49 +0700

    txt parser from py to str

--- pytostr.py
+++ pytostr.py
@@ -1,2 +1,30 @@
-with open('function.txt') as f:
-    lines = f.readlines()
+def pyToStr(file):
+    with open(file) as f:
+        lines = f.readlines()
+        element = []
+        for line in lines:
+            #mereplace string dri beberapa symbol agar dapat di split dengan space 
+            line = line.replace('(',' ( ')
+            line = line.replace(')',' ) ')
+            line = line.replace('[',' [ ')
+            line = line.replace(']',' ] ')
+            line = line.replace('+',' + ')
+            line = line.replace('-',' - ')
+            line = line.replace('*',' * ')
+            line = line.replace(':',' : ')
+            line = line.replace('/',' / ')
+            line = line.replace('>',' > ')
+            line = line.replace('<',' < ')
+            line = line.replace('"',' " ')
+            line = line.replace("'"," ' ")
+            line = line.replace(',',' , ')
+            line = line.replace('.',' . ')
+            line = line.replace('%',' % ')
+            line = line.replace('=',' = ')
+            line = line.split()
+            element.append(line)
+        return element
+
+file = 'function.txt'
+print(pyToStr(file))
+
