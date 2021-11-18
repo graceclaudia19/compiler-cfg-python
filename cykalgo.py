8cdcaf79c2e18b2dd76a1bfbd2915e5ae2fef3b9
graceclaudia19 <graceclaudia78@gmail.com>
2021-11-18 08:19:13 +0700

    cyk algo (kasar) done

--- cykalgo.py
+++ cykalgo.py
@@ -19,7 +19,8 @@ def get_key(gramParsed, value, elementM):
     for key, values in gramParsed.items():
         for val in values:
             if val == value:
-                elementM.append(key)
+                if key not in elementM:
+                    elementM.append(key)
     return elementM
 
 def cykalgo(gramParsed, word):
@@ -27,11 +28,6 @@ def cykalgo(gramParsed, word):
     blank = 0
     rules = gramParsed
     keyRules = {}
-    # membalik key-value menjadi value-key agar lebih mudah dalam pengerjaan:
-    # for key,value in rules.items():
-    #     for val in value:
-    #         keyRules[val] = key
-    # print(keyRules)
 
     for i in range (len(word)+1):
         #blank akan ditambah 1 saat bertambah linenya
@@ -50,105 +46,59 @@ def cykalgo(gramParsed, word):
                             el.append(key)
                             table[1][j] = el
                             keyRules[val] = el
-            elif (i==2):
-                #string merupakan ba,aa,ab,ba,baa,aab,aba,baab,aaba,baaba
-                str = word[j:j+i]
-                #dalam indexing string, jika mis: [a:b] jika b melebihi len dari str tersebut akan ttp keluar dgn elemen yang kurang dri semestinya
-                if len(str) == i:
-                    #jika string sesuai
-                    print(str)
-                    #mencari substring dri string
-                    substrings = []
-                    for x in range (i):
-                        substr = str[x:x+i-1]
-                        if (len(substr) == i-1):
-                            # jika panjang benar akan diappend ke substr
-                            substrings.append(substr)
-                    print(substrings)
-                    # ini merupakan pengingat terminal di atasnya yang pernah dilewati agar tidak mengulang pencarian
-                    keySubstr = []
-                    #untuk melakukan perkalian silang antar elemen dibutuhkan parsing tingkat sebelumnya dari substring tersebut (mis: b jadi B a jadi A, C)
-                    for substr in substrings:
-                        keySubstr.append(keyRules[substr])
-                    print(keySubstr)
-                    # perkalian silang antar parsingan elemen misalnya BA dan BC
-                    elements = [a+b for a in keySubstr[0] for b in keySubstr[1]]
-                    print(elements)
-                    elementM = []
-                    for el in elements:
-                        # jika value ada di grammar
-                        if (check_value_grammar(rules, el)):
-                            #append key ke element dari table
-                            get_key(rules, el, elementM)
-                        # jika element ada di parsingan kata dari sebelumnya, ambil keynya (terminal)
-                        elif (el in list(keyRules.keys())):
-                            elementM.append(keyRules[el])
-                        else:
-                        # selain itu pass
-                            pass
-                    #pengisian elemen ke dalam tabel
-                    table[i][j] = elementM
-                    # menambahkan info terminal elemen tersebut untuk elemen selanjutnya
-                    keyRules[str] = elementM
-                    print(keyRules)
             else:
                 #string merupakan ba,aa,ab,ba,baa,aab,aba,baab,aaba,baaba
                 str = word[j:j+i]
                 #dalam indexing string, jika mis: [a:b] jika b melebihi len dari str tersebut akan ttp keluar dgn elemen yang kurang dri semestinya
                 if len(str) == i:
                     #jika string sesuai
-                    print(str)
                     #mencari substring dri string
                     substrings = []
-                    for x in range (i):
-                        substr = str[x:x+i-1]
-                        if (len(substr) == i-1):
-                            # jika panjang benar akan diappend ke substr
-                            substrings.append(substr)
-                    print(substrings)
-                    # ini merupakan pengingat terminal di atasnya yang pernah dilewati agar tidak mengulang pencarian
-                    keySubstr = []
-                    # element dalam m nanti yg digunakan
+                    #untuk elemen dari tabel
                     elementM = []
-                    #untuk melakukan perkalian silang antar elemen dibutuhkan parsing tingkat sebelumnya dari substring tersebut (mis: b jadi B a jadi A, C)
-                    for substr in substrings:
-                        keySubstr.append(keyRules[substr])
-                    print(keySubstr)
-                    # perkalian silang antar parsingan elemen misalnya BA dan BC
-                    elements = [a+b for a in keySubstr[0] for b in keySubstr[1]]
-                    print(elements)
-                    for el in elements:
-                        # jika value ada di grammar
-                        if (check_value_grammar(rules, el)):
-                            #append key ke element dari table
-                            get_key(rules, el, elementM)
-                        # jika element ada di parsingan kata dari sebelumnya, ambil keynya (terminal)
-                        elif (el in list(keyRules.keys())):
-                            elementM.append(keyRules[el])
-                        else:
-                        # selain itu pass
-                            pass
-                    #pengisian elemen ke dalam tabel
+                    # inisiasi x dengan 0 untuk pemisah substring
+                    x = 0
+                    for a in range (len(str)):
+                        #element depan dari substring
+                        front = str[:x]
+                        #element belakang dari substring
+                        other = str[x:]
+                        
+                        #pengecekan apabila substring tidak kosong atau tidak sama panjangnya dengan kata aslinya
+                        if (len(front)!=0 and len(other)!=0 and len(front)!=i and len(other)!=i):
+                            substrings.append(front)
+                            substrings.append(other)
+                        #penambahan x setiap kali menambah panjang substring depan
+                        x+=1
+                    #untuk setiap substring akan di iterate dan dicari terminalnya
+                    for sub in range (0,len(substrings),2):
+                        keySubstr = []
+                        keySubstr.append(keyRules[substrings[sub]])
+                        keySubstr.append(keyRules[substrings[sub+1]])
+                        # perkalian silang antar parsingan elemen misalnya BA dan BC
+                        elements = [a+b for a in keySubstr[0] for b in keySubstr[1]]
+                        for el in elements:
+                            # jika value ada di grammar
+                            if (check_value_grammar(rules, el)):
+                                #append key ke element dari table
+                                get_key(rules, el, elementM)
+                            # jika element ada di parsingan kata dari sebelumnya, ambil keynya (terminal)
+                            elif (el in list(keyRules.keys())):
+                                if (el not in elementM):
+                                    elementM.append(keyRules[el])
+                            else:
+                            # selain itu pass
+                                pass
                     table[i][j] = elementM
-                    # menambahkan info terminal elemen tersebut untuk elemen selanjutnya
+                        # menambahkan info terminal elemen tersebut untuk elemen selanjutnya
                     keyRules[str] = elementM
-                    print(keyRules)
-
-
-    print(keyRules)
-    print()
     return table
-                
 
-
-
-
-
-# print(grammarParse('grammar.txt'))
-# cyk = DefaultDict(set)
-# cyk[0].add(1)
-# cyk[0].add(2)
-# print(cyk)
-# print(cyk[0])
 gram = grammarParse('grammar.txt')
-print(cykalgo(gram,"baaba"))
+word = "baaba"
+table = cykalgo(gram,word)
+
+top = table[len(word)][0]
+for i in top:
+    if i == 'S':
+        print("yey di acc")
