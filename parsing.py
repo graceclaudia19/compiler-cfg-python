2ae27bbc3138548afa1ad2ff6909608eea4bf9b7
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2021-11-24 11:28:09 +0700

    Fixing varChecker

--- parsing.py
+++ parsing.py
@@ -3,10 +3,10 @@ from cyk import grammarParse
 #FUNCTION PENGECEKAN NAMA VARIABEL
 def varChecker(varName):
     # pengecekan huruf pertama dari nama variabel (hanya boleh huruf dan _ )
-    if (varName[0] == '_' or (ord(varName[0])>=65 and ord(varName[0])<=122)):
+    if (varName[0] == '_' or (ord(varName[0])>=65 and ord(varName[0])<= 90) or (ord(varName[0]) >= 97 and ord(varName[0]) <= 122)):
         for i in range (1,len(varName)):
              # pengecekan sisa hurruf selain huruf pertama dari nama variabel menggunakan ascii value (hanya boleh huruf, _, dan angka )
-            if not (varName[i]== "_" or (ord(varName[0])<=65) or (ord(varName[0])>=122) or (ord(varName[0])<=57) or (ord(varName[0])>=48)):
+            if not (varName[i] == '_' or (ord(varName[i]) >= 48 and ord(varName[i]) <= 57) or (ord(varName[i])>=65 and ord(varName[i])<= 90) or (ord(varName[i]) >= 97 and ord(varName[i]) <= 122)):
                 return False
         return True
     return False
